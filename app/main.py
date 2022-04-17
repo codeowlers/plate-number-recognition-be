be9587cc5d58b442087b77569e60f179c854f240
developer-sidani <developer.sidani@gmail.com>
2022-04-17 22:30:08 +0300

    added await

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import asyncio
+import time
 
 app = FastAPI()
 origins = ["*"]
@@ -20,7 +22,11 @@ def read_root():
 
 @app.get("/run")
 async def read_item(url: str, token: str):
+    received_url = 'url+"&token="+token'
+    new_url = 'https://firebasestorage.googleapis.com/v0/b/learn-plus-fyp.appspot.com/o/python%2F5735505-52d1-1d20-c6c5' \
+              '-aec1d1dea83-Photo%20-%20Ahmad%20Sidani.jpg?alt=media&token=0ccd9681-c6d7-4cec-9da7-4473e737b085 '
+    await asyncio.sleep(2000)
     return {
-        "url": url+"&token="+token,
+        "url": new_url,
         "plateNumber": 1234
     }
